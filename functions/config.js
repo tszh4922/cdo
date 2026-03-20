8291cfc2b29c10990bc80e21bce5684fd0b20582
tszh4922 <tszh4922@gmail.com>
2026-03-20 14:47:15 +0800

    Create config.js

---
+++ functions/config.js
@@ -0,0 +1,28 @@
+export let CONFIG = {
+
+  password: "admin123", // 后台密码（一定要改）
+
+  upstreams: {
+    global: [
+      "https://cloudflare-dns.com/dns-query",
+      "https://dns.google/dns-query"
+    ],
+    backup: [
+      "https://1.0.0.1/dns-query"
+    ]
+  },
+
+  blocklist: [
+    "doubleclick.net",
+    "ads.google.com",
+    "tracking."
+  ],
+
+  chinaDomains: [
+    ".cn",
+    "baidu.com",
+    "qq.com"
+  ],
+
+  cacheTTL: 120
+}
